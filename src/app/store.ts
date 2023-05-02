2f9d5bfe143b5fc625b55d249de9a5b8b162a3ae
Nika Kopadze <nikolozkopadze@gmail.com>
2023-05-02 21:41:38 +0400

    minor changes

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,19 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from '../features/themeSlice';
 import dictionaryReducer from '../features/dictionarySlice';
 import fontReducer from '../features/fontSlice';
-import { wordApi } from '../services/useGetWordQuery';
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     dictionary: dictionaryReducer,
     font: fontReducer,
-
-    [wordApi.reducerPath]: wordApi.reducer,
   },
-
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(wordApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
